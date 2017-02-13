bcca7e5c8d9b14b99649bbc1c046f08b165a62c3
anders chen <chenhf2010@qq.com>
2017-02-13 10:51:42 +0800

    添加项目文件。

---
+++ Direct2DDemo/Direct2DDemo.h
@@ -0,0 +1,32 @@
+
+// Direct2DDemo.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CDirect2DDemoApp: 
+// 有关此类的实现，请参阅 Direct2DDemo.cpp
+//
+
+class CDirect2DDemoApp : public CWinApp
+{
+public:
+	CDirect2DDemoApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDirect2DDemoApp theApp;
